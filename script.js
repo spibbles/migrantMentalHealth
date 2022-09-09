4684d21aa1f7e31d305e4e74289710b323fb530a
tumi 3 <94874741+spibbles@users.noreply.github.com>
2022-09-08 19:32:48 -0600

    Fixing a typo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload=function(){
               "Check in, Check in, Check in! Regularly check in with your loved ones, and show them that you care.",
               "Make plans together. Ask your loved one to go on walks, to help you with a hobby, or do an activity they used to like",
   "Don't give up on them, even if they push you away. It makes sense to feel frustrated if your loved one refuses to get help, but it's important to maintain communication lines with them",
-              "Encourage them to seek professional help. You can't solve all you're loved one's issues, but you can take them to someone who can",
+              "Encourage them to seek professional help. You can't solve all your loved one's issues, but you can take them to someone who can",
               "Be willing to listen when your loved ones open up to you. It's important to actively and patienty listen - even if its something you don't want to hear.  It can be difficult for a person struggling with a mental illness to talk about their experiences, when they do feel comfortable sharing their feelings with you, cherish it, and hold your criticisms."
               ]
 
